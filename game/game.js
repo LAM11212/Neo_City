ed78f3cfa34a8dc9f591c4de2e89576297f6807d
Levi <leviamullins@gmail.com>
2026-02-15 21:28:01 -0500

    somewhat working game

--- game/game.js
+++ game/game.js
@@ -1,25 +1,50 @@
+let screenRight = 600;
+
 class Example extends Phaser.Scene {
     preload() {
         this.load.image('background', 'assets/space.png');
         this.load.image('player', 'assets/player.png');
+        this.load.image('obstacle', 'assets/testObstacle.png');
     }
 
     create() {
         this.add.image(0, 0, 'background').setScale(1.3);
+        this.obstacles = this.physics.add.group();
+        
         this.player = this.physics.add.sprite(50, 50, 'player').setScale(2);
         this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.time.addEvent({
+            delay: 1500,
+            callback: this.generateNewObstacle,
+            callbackScope: this,
+            loop: true
+        });
     }
 
     update() {
-        if(this.cursors.left.isDown) {
-            this.player.setVelocityX(-160);
-        } else if(this.cursors.right.isDown) {
-            this.player.setVelocityX(160);
-        } else if(this.cursors.up.isDown) {
-            this.player.setVelocityY(-160);
-        } else if(this.cursors.down.isDown) {
-            this.player.setVelocityY(160);
-        }
+        if(this.cursors.space.isDown) {
+            this.player.setVelocityY(-200);
+        } 
+
+        this.obstacles.getChildren().forEach((block) => {
+            if(block.x < -50) {
+                block.destroy();
+            }
+        });
+ 
+    }
+
+    generateNewObstacle() {
+        let minY = 50;
+        let maxY = 550;
+        let y = Phaser.Math.Between(minY, maxY);
+
+        let block = this.obstacles.create(screenRight, y, 'obstacle');
+        block.body.allowGravity = false;
+        block.setVelocityX(-200);
+
+        return block;
     }
 }
 
@@ -30,7 +55,7 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            gravity: { y: 400 },
             debug: false
         }
     },
